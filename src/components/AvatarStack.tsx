68d6f5700f8ce993bf7099123bf41857d94a0501
Arti Mathanda <arti.mathanda@ably.com>
2022-07-15 14:21:25 +0100

    feat: Single user Avatar stack

--- src/components/AvatarStack.tsx
+++ src/components/AvatarStack.tsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import { useState } from 'react'
 
-const AvatarStack = () => <h1>I be stackin' those avatars</h1>;
-
-export default AvatarStack;
+const AvatarStack = () => {
+  const [hovered, setHovered] = useState(false)
+  return (
+    <div className="flex flex-col">
+      <div
+        className="bg-gradient-to-r from-cyan-500 to-blue-500 
+				h-12 w-12 rounded-full mb-8
+				hover:w-14 hover:h-14 hover:border-2  hover:bg-blue-900"
+        onMouseEnter={() => setHovered(!hovered)}
+        onMouseLeave={() => setHovered(!hovered)}
+      ></div>
+      {hovered ? (
+        <p className="h-8 w-14 bg-black rounded-lg text-white text-center py-1">
+          You
+        </p>
+      ) : null}
+    </div>
+  )
+}
+export default AvatarStack
