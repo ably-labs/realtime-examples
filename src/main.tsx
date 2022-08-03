6db904e118f09dd8aef0be4464e8ecf5ea8a5441
Arti Mathanda <arti.mathanda@ably.com>
2022-08-03 13:08:33 +0100

    chore: Removes unused or implicit imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-import { nanoid } from 'nanoid'
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './Layout'
 import AvatarStack from './components/AvatarStack'
 import Home from './home'
