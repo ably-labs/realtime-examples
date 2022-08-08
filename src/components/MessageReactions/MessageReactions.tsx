5873897af85c66e01352612ea4251fb1a951913e
Peter Maguire <peter.maguire@ably.com>
2022-08-08 16:23:54 +0100

    properly center add reaction button

--- src/components/MessageReactions/MessageReactions.tsx
+++ src/components/MessageReactions/MessageReactions.tsx
@@ -222,7 +222,7 @@ const MessageReactions = () => {
                 </ul>
               ) : null}
               <EmojiHappyIcon
-                className="inline-block ml-2 h-5 w-5 text-slate-500"
+                className="inline-block ml-2 h-5 w-5 text-slate-500 self-center"
                 onClick={() => setShowEmojiList(!showEmojiList)}
               />
             </div>
