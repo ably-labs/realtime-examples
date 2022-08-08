6366582e6ad63f0357cc44b7d74df460d961e229
Arti Mathanda <arti.mathanda@ably.com>
2022-08-08 11:09:59 +0100

    fix: Makes MouseOver work for UserIcons

--- src/components/AvatarStack/AvatarStack.tsx
+++ src/components/AvatarStack/AvatarStack.tsx
@@ -120,7 +120,7 @@ const AvatarStack = () => {
   return (
     <div className="w-screen flex justify-between px-6 md:max-w-lg md:-mt-32">
       <div className="group relative flex flex-col items-center group">
-        <UserCircleIcon className="absolute mt-2 h-8 w-8 opacity-80 text-white" />
+        <UserCircleIcon className="absolute mt-2 h-8 w-8 opacity-80 text-white pointer-events-none" />
         <div
           className="bg-gradient-to-r from-cyan-500 to-blue-500 
                 h-12 w-12 rounded-full mb-2"
@@ -148,7 +148,7 @@ const AvatarStack = () => {
                   zIndex: otherUsers.length - index,
                 }}
               >
-                <UserCircleIcon className="absolute mt-2 h-8 w-8 opacity-80 stroke-white fill-transparent" />
+                <UserCircleIcon className="absolute mt-2 h-8 w-8 opacity-80 stroke-white fill-transparent pointer-events-none" />
                 <div
                   className={`bg-gradient-to-l ${colours[index]} h-12 w-12 rounded-full mb-2 shadow-[0_0_0_4px_rgba(255,255,255,1)]`}
                   onMouseOver={() => setHoveredClientId(user.clientId)}
