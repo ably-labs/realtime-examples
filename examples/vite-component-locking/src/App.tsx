8674035a684a2088c30b5c1b9ba7d6b16613fa7c
Dominik Piatek <do.piatek@gmail.com>
2023-11-02 12:41:04 +0000

    Fix space name not being generated

--- examples/vite-component-locking/src/App.tsx
+++ examples/vite-component-locking/src/App.tsx
@@ -1,10 +1,13 @@
 import Spaces from "@ably/spaces";
 import { SpaceProvider, SpacesProvider } from "@ably/spaces/react";
 import ComponentLocking from "./components/ComponentLocking";
+import { getSpaceNameFromUrl } from "./utils/helpers";
+
+const spaceName = getSpaceNameFromUrl();
 
 const App = ({ spaces }: { spaces: Spaces }) => (
   <SpacesProvider client={spaces}>
-    <SpaceProvider name="component-locking">
+    <SpaceProvider name={spaceName}>
       <ComponentLocking />
     </SpaceProvider>
   </SpacesProvider>
--- examples/vite-member-location/src/App.tsx
+++ examples/vite-member-location/src/App.tsx
@@ -1,10 +1,13 @@
 import type Spaces from "@ably/spaces";
 import { SpaceProvider, SpacesProvider } from "@ably/spaces/react";
 import MemberLocation from "./components/MemberLocation";
+import { getSpaceNameFromUrl } from "./utils/helpers";
+
+const spaceName = getSpaceNameFromUrl();
 
 const App = ({ spaces }: { spaces: Spaces }) => (
   <SpacesProvider client={spaces}>
-    <SpaceProvider name="member-location">
+    <SpaceProvider name={spaceName}>
       <MemberLocation />
     </SpaceProvider>
   </SpacesProvider>
